c5a92cb56101f552a4f348fed81ae51137b21417
Andrew Bradley <cspotcode@gmail.com>
2023-05-06 21:40:09 -0400

    fix

---
+++ src/ava.ts
@@ -0,0 +1,11 @@
+// https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1519138189
+export {
+  default,
+  ExecutionContext,
+  Implementation,
+  ImplementationFn,
+  Macro,
+  MacroDeclarationOptions,
+  MacroFn,
+  TestFn,
+} from 'ava';
